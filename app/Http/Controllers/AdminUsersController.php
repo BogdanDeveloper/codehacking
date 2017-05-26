01e6515fcdeab52f60e23ddd53a7113024ba395e
Bogdan <bogdan@gmail.com>
2017-05-26 12:40:16 +0200

    Users with uploading images

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Role;
+use App\Photo;
 use App\Http\Requests\UsersCreateRequest;
 
 use Illuminate\Http\Request;
@@ -48,7 +49,22 @@ class AdminUsersController extends Controller
      */
     public function store(UsersCreateRequest $request)
     {
-        User::create($request -> all());
+        $input = $request -> all();
+
+        if ($file = $request -> file('photo_id')) {
+            $name = time() . $file -> getClientOriginalName();
+
+            $file -> move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo -> id;
+        }
+
+        $input['password'] = bcrypt($request -> password);
+
+        User::create($input);
+
 
         return redirect('admin/users');
     }
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 #Route::resource('/admin/users', 'AdminUsersController@index');
 
-Route::get('/admin/users', 'AdminUsersController@index');
+Route::get('/admin/users', 'AdminUsersController@index') -> name('all_users');
 
-Route::get('/admin/users/create', 'AdminUsersController@create');
+Route::get('/admin/users/create', 'AdminUsersController@create') -> name('create_users');
 
 Route::post('/admin/users/store', 'AdminUsersController@store');
 
